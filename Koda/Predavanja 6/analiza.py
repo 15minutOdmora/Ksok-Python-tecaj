c1de71b12e987fe290106746ded94345b73705b0
15minutOdmora <liammislej@gmail.com>
2021-05-25 13:22:09 +0200

    Dodal slike.

--- Koda/Predavanja 6/analiza.py
+++ Koda/Predavanja 6/analiza.py
@@ -16,21 +16,53 @@ def izrisi_gdp():
         plt.show()
 
 
-
-
 def izrisi_preb():
     with open("preb_po_star_in_spol.csv", "r") as f:
         data = list(csv.reader(f))
         cleaned = {"Moški": {}, "Ženske": {}}
+        print(data)
         for row in data[3:]:
-            cleaned[row[0]][row[1]] = row[2:]
-            print(row)
 
+            uporabni = row[2:5] + row[11:13]
+            podatki = [int(i) for i in uporabni]
+            cleaned[row[0]][row[1]] = podatki
 
-x = [0, 1, 2, 3, 4]
-y = [2, 3, 2, 3, 1]
-plt.plot(x, y)
-plt.show()
+        # Seštejemo za moške in ženske
+        moski = sum(cleaned["Moški"]["SLOVENIJA"])
+        zenske = sum(cleaned["Ženske"]["SLOVENIJA"])
+        skupaj = moski + zenske
+        print(moski, zenske, skupaj)
+        # Izpišemo
+        print("V Sloveniji je {} prebivalcev od tega je {}% moških in {}% žensk".format(skupaj,
+                                                                               round((moski/skupaj) * 100, 2),
+                                                                                          round((zenske/skupaj) * 100, 2)))
+        # Določimo seznam za posamezen stolpec v histogramu
+        ticks = ["0 let", "1-5 let", "6-14 let", "15-64 let", "65 let ali več"]
+        bars_x = [1, 2, 3, 4, 5]
+
+        moski_po_letih = cleaned["Moški"]["SLOVENIJA"]
+        zenske_po_letih = cleaned["Ženske"]["SLOVENIJA"]
+        m_in_z = [moski_po_letih, zenske_po_letih]
+
+        moski_in_zenske = [moski_po_letih[i] + zenske_po_letih[i] for i in range(len(moski_po_letih))]
+        plt.bar(bars_x, height=moski_in_zenske, color="blue")
+        # plt.bar(bars_x, height=moski_in_zenske, color="blue")
+        plt.bar(bars_x, height=zenske_po_letih, color="red")
+        plt.xticks(bars_x, ticks)
+        plt.show()
+
 
 if __name__ == "__main__":
-    izrisi_preb()
+    # izrisi_preb()
+    visine_igralcev = [183, 185, 188, 193, 196, 198, 201, 206, 208, 213, 221, 224]  # V cm
+
+    bars_x = ["< 190", "190 - 200", "200-210", "210 <"]  # Opisi stolpcev
+    st_po_visini = [3, 2, 3, 3]  # 3 igralci so nižji od 190cm, 2 med 190cm in 200cm, ...
+
+    plt.bar(bars_x, height=st_po_visini)
+    # Nastavimo opise in naslov
+    plt.xlabel("Višina v cm")
+    plt.ylabel("Število igralcev")
+    plt.title("Višine košarkašev skupine Dallas Mavericks")
+    # Izrišemo
+    plt.show()
